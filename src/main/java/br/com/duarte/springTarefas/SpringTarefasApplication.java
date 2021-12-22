2d8e2d2fd86f286eb3d27bdb52c71fcc15fec8cc
phduart <49646944+phduart@users.noreply.github.com>
2021-12-22 17:11:58 -0300

    Subindo projeto zerado com starter web

---
+++ src/main/java/br/com/duarte/springTarefas/SpringTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.duarte.springTarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/duarte/springTarefas/SpringTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duarte.springTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
